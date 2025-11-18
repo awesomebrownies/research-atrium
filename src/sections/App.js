f46bfafc340ce5c184dbfed0b2d3fd5f845a3056
awesomebrownies <evan.n.brown25@gmail.com>
2025-11-17 22:21:20 -0700

    Remove redundant code

--- src/sections/App.js
+++ src/sections/App.js
@@ -1,17 +1,6 @@
-import React, { Component } from 'react';
 import '../App.css';
-import FlagOnMountain from '../assets/images/flag-on-mountain.png';
-import MountainForeground from '../assets/images/mountain-foreground.png';
-import Name from '../assets/images/name.png';
-import Clouds from '../assets/images/clouds.png';
-import MinecraftLogo from '../assets/images/minecraft_logo.png';
-import Rocks from '../assets/images/foreground.png';
-import { FaLinkedin, FaGithub } from "react-icons/fa";
-import { MdEmail } from "react-icons/md";
-
 import '../index.css';
 
-import Parallax from './Parallax';
 import Cover from './Cover';
 import TableOfContents from './TableOfContents';
 import Projects from './Projects';
@@ -21,16 +10,12 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <div>
           <Cover/>
-          <div className="parallax-projects"> 
-            <div className="app-projects">
-              <TableOfContents/>
-              <Projects/>
-              <Footer/>
-            </div>
+          <div className="parallax-projects app-projects"> 
+            <TableOfContents/>
+            <Projects/>
+            <Footer/>
           </div>
-        </div>
       </div>
     );
   }
