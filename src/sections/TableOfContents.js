9ec051ff00e4311b1afa08fe7a4ea4d483053665
awesomebrownies <evan.n.brown25@gmail.com>
2025-11-17 23:22:42 -0700

    Use data driven TOC

--- src/sections/TableOfContents.js
+++ src/sections/TableOfContents.js
@@ -2,62 +2,63 @@ import '../App.css';;
 import '../index.css';
 
 const TableOfContents = () => {
-    return (
-        <div>
-            <ul className="project-body list">
-                <li>
-                  <a href="#Server-Side_Mods" className="link"><span className="number">1</span><span>Server-Side Mods</span></a>
-                  <ul className="list">
-                    <li>
-                      <a href="#Infiltration" className="link"><span className="number">1.1</span><span>Infiltration</span></a>
-                      <ul className="list">
-                        <li>
-                          <a href="#Firearms" className="link"><span className="number">1.1.1</span><span>Firearms</span></a>
-                        </li>
-                        <li>
-                          <a href="#Helicopters" className="link"><span className="number">1.1.2</span><span>Helicopters</span></a>
-                        </li>
-                        <li>
-                          <a href="#Spatial_Interface" className="link"><span className="number">1.1.3</span><span>Spatial Interface</span></a>
-                        </li>
-                        <li>
-                          <a href="#Screen_Effects" className="link"><span className="number">1.1.4</span><span>Screen Effects</span></a>
-                        </li>
-                      </ul>
-                    </li>
-                    <li>
-                      <a href="#Project:_MTF" className="link"><span className="number">1.2</span><span>Project: MTF</span></a>
-                      <ul className="list">
-                        <li>
-                          <a href="#Story" className="link"><span className="number">1.2.1</span><span>Story</span></a>
-                        </li>
-                      </ul>
-                    </li>
-                    <li>
-                      <a href="#Unsecured" className="link"><span className="number">1.3</span><span>Unsecured</span></a>
-                      <ul className="list">
-                        <li>
-                          <a href="#Anomalies" className="link"><span className="number">1.3.1</span><span>Anomalies</span></a>
-                        </li>
-                        <li>
-                          <a href="#Map" className="link"><span className="number">1.3.2</span><span>Map</span></a>
-                        </li>
-                      </ul>
-                    </li>
-                  </ul>
-                </li>
-                
-                <li>
-                  <a href="#Games" className="link"><span className="number">2</span><span>Games</span></a>
-                  <ul className="list">
-                    <li>
-                      <a href="#SCP:_Descent" className="link"><span className="number">2.1</span><span>SCP: Descent</span></a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-        </div>
-    );
+  const tocData = [
+    {
+      id: "Server-Side_Mods",
+      number: "1",
+      title: "Server-Side Mods",
+      children: [
+        {
+          id: "Infiltration",
+          number: "1.1",
+          title: "Infiltration",
+          children: [
+            {id: "Firearms", number: "1.1.1", title: "Firearms"},
+            {id: "Helicopters", number: "1.1.2", title: "Helicopters"},
+            {id: "Spatial_Interface", number: "1.1.3", title: "Spatial Interface"},
+            {id: "Screen_Effects", number: "1.1.4", title: "Screen Effects"}
+          ]
+        },
+        {
+          id: "Project:_MTF",
+          number: "1.2",
+          title: "Project: MTF",
+          children: [
+            {id: "Story", number: "1.2.1", title: "Story"}
+          ]
+        },
+        {
+          id: "Unsecured",
+          number: "1.3",
+          title: "Unsecured",
+          children: [
+            {id: "Anomalies", number: "1.3.1", title: "Anomalies"},
+            {id: "Map", number: "1.3.2", title: "Map"}
+          ]
+        }
+      ]
+    }
+  ]
+
+  const renderItem = (item) => (
+    <li>
+      <a href={`#${item.id}`} className="link">
+        <span className="number">{item.number}</span>
+        <span>{item.title}</span>
+      </a>
+      {item.children && (
+        <ul className="list">{item.children.map(renderItem)}</ul>
+      )}
+    </li>
+  );
+
+  return (
+    <div>
+      <ul className="project-body list">
+        {tocData.map(renderItem)}
+      </ul>
+    </div>
+  );
 }
 
 export default TableOfContents;
