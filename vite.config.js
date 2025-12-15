6165970c4aa7d0e8552158aabde8f0de56890303
awesomebrownies <evan.n.brown25@gmail.com>
2025-12-14 20:20:20 -0700

    Update URL

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/research-atrium/', // Replace with your actual GitHub repo name
+  base: '/',
 })
